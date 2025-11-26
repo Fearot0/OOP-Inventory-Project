d2c1424ee1c2c3e69dbbc25efbe647ebcfca062e
Rohat <rohatharman34@gmail.com>
2025-11-26 21:48:22 +0300

    Add PerishableProduct class

---
+++ src/inventory/PerishableProduct.java
@@ -0,0 +1,26 @@
+package inventory;
+
+import java.time.LocalDate;
+
+
+public class PerishableProduct extends Product {
+
+    private LocalDate expiryDate;
+
+    public PerishableProduct(int id, String name, double price, int stockQuantity, LocalDate expiryDate){
+        super(id, name, price, stockQuantity);
+        this.expiryDate = expiryDate;
+    }
+
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+
+}
